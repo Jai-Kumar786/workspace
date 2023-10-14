7964e0dddd8f5c2343ba5c152f27fcabe6f8ece7
CodeSandbox <support@codesandbox.io>
2023-10-14 18:21:39 +0000

    Sandbox snapshot commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Alert from "./component/Alert";
+function App() {
+  return (
+    <div className="App">
+      <Alert type="error" message="Error" />
+      <Alert type="success">
+        <p>Success message</p>
+      </Alert>
+      <Alert type="primary">
+        <h4>primary message</h4>
+      </Alert>
+      <Alert type="secondary">
+        <span>secondary message</span>
+      </Alert>
+    </div>
+  );
+}
+export default App;
